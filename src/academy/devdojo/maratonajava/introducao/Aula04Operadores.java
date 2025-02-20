560e1ab085e9c6cdbaa3f0ae808e33699857646c
faleite <fabricio_2310@hotmail.com>
2025-02-20 22:29:30 +0000

    Add Aula04Operadores.java to demonstrate operator usage

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        int number01 = 10;
+        int number02 = 20;
+        int result = number01 + number02;
+        System.out.println(result);
+
+        // Os dois primeiros números (number01 + number02) são somados porque o operador '+'
+        // é usado aritmeticamente (10 + 20 = 30).
+        // A partir de "Valor", o operador '+' é usado para concatenação, resultando em "30 Valor".
+        // Então number01 e number02 (10 e 20) são convertidos para String e concatenados, formando "1020".
+        // O resultado final é "30 Valor 1020".
+        System.out.println(number01 + number02 + " Valor " + number01 + number02); // "30 Valor 1020".
+        System.out.println(number01 + number02 + " Valor " + (number01 + number02)); // "30 Valor 30".
+    }
+}
