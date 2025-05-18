d5f333557f1ff5583a57a2d810e343be8abdb699
faleite <fabricio_2310@hotmail.com>
2025-05-18 12:53:31 +0100

    fix some bugs on game

--- java-basico/java/src/com/faleite/cursojava/aula20/Ex06.java
+++ java-basico/java/src/com/faleite/cursojava/aula20/Ex06.java
@@ -22,14 +22,15 @@ public class Ex06 {
            return true;
        else return game[0][2] == player && game[1][1] == player && game[2][0] == player;
     }
+
     public static boolean validNumber(byte num){
-        return num < 0 || num > 2;
+        return num < 1 || num > 3;
     }
 
     public static void showGame(char[][] game){
         for (char[] chars : game) {
             for (char aChar : chars) {
-                System.out.print(aChar + " ");
+                System.out.print(aChar + " | ");
             }
             System.out.println();
         }
@@ -46,6 +47,7 @@ public class Ex06 {
         char[][] game = new char[3][3];
         boolean win = false;
         char player = 'X';
+        byte tie = 0;
 
         fillGame(game);
         System.out.println("Jogo da Velha");
@@ -57,12 +59,16 @@ public class Ex06 {
 
             System.out.println("E a vez de " + player);
             while (validNumber(line)) {
-                System.out.println("Digite a posição da linha: ");
+                System.out.println("Digite a posição da linha (1-3): ");
                 line = scan.nextByte();
             }
             while (validNumber(column)) {
-                System.out.println("Digite a posição da coluna: ");
+                System.out.println("Digite a posição da coluna (1-3): ");
                 column = scan.nextByte();
+            }
+            if (game[--line][--column] != '_'){
+                System.out.println("Posição ja usada, tente novamente.");
+                continue;
             }
             game[line][column] = player;
             if (winner(game, player)) {
@@ -70,6 +76,11 @@ public class Ex06 {
                 win = true;
             }
             showGame(game);
+            tie++;
+            if (tie > 8){
+                System.out.println("Não houve vencedor!!!");
+                break;
+            }
             player = (player == 'X') ? 'O' : 'X';
         }
     }
