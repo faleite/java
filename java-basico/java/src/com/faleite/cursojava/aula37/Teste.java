89a9b26329ce06814b2b3adf3a76f6c4ef029683
faleite <fabricio_2310@hotmail.com>
2025-06-03 18:51:31 +0100

    Add Pessoa, Aluno, and Professor classes with Teste example
    Introduced `Pessoa` as a base class with common attributes. Added `Aluno` and `Professor` classes extending `Pessoa`, including specific attributes and methods. Added `Teste` class demonstrating polymorphism with these classes.

---
+++ java-basico/java/src/com/faleite/cursojava/aula37/Teste.java
@@ -0,0 +1,15 @@
+package com.faleite.cursojava.aula37;
+
+public class Teste {
+    public static void main(String[] args) {
+       /* Aluno aluno = new Aluno();
+        Professor professor = new Professor();
+        Pessoa pessoa = new Pessoa();
+
+        aluno.setNome("Bento");
+        professor.setTelefone("111-111-111");*/
+
+        // Super classe
+        Pessoa aluno = new Aluno();
+    }
+}
